ac8c76dc5862c663a211a8dd46995f4329591b7e
bryanares <bryanares12@gmail.com>
2021-03-07 17:31:29 +0300

    added a js function to add selected items to cart

--- js/main.js
+++ js/main.js
@@ -26,3 +26,11 @@ function ready() {
 		.getElementsByClassName("btn-purchase")[0]
 		.addEventListener("click", purchaseClicked);
 }
+function purchaseClicked() {
+	alert("Thank you for your purchase");
+	var cartItems = document.getElementsByClassName("cart-items")[0];
+	while (cartItems.hasChildNodes()) {
+		cartItems.removeChild(cartItems.firstChild);
+	}
+	updateCartTotal();
+}
