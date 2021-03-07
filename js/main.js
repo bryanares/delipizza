40d91a7ec7f5e813fc86f49637657d4b0c09f2ca
bryanares <bryanares12@gmail.com>
2021-03-07 17:34:13 +0300

    added a js function to manipulate the cart to compute total cost of items

--- js/main.js
+++ js/main.js
@@ -87,3 +87,21 @@ function addItemToCart(title, price, imageSrc) {
 		.getElementsByClassName("cart-quantity-input")[0]
 		.addEventListener("change", quantityChanged);
 }
+function updateCartTotal() {
+	var cartItemContainer = document.getElementsByClassName("cart-items")[0];
+	var cartRows = cartItemContainer.getElementsByClassName("cart-row");
+	var total = 0;
+	for (var i = 0; i < cartRows.length; i++) {
+		var cartRow = cartRows[i];
+		var priceElement = cartRow.getElementsByClassName("cart-price")[0];
+		var quantityElement = cartRow.getElementsByClassName(
+			"cart-quantity-input"
+		)[0];
+		var price = parseFloat(priceElement.innerText.replace("Ksh. ", ""));
+		var quantity = quantityElement.value;
+		total = total + price * quantity;
+	}
+	total = Math.round(total * 100) / 100;
+	document.getElementsByClassName("cart-total-price")[0].innerText =
+		"Ksh. " + total;
+}
